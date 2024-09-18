93c6d694a3d87867bdae7d60005b415d820824e2
tombtiml <henrik.lieb@thg-hn.de>
2024-09-18 11:15:23 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,10 @@
 input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
     if (input.temperature() > 25) {
         basic.setLedColor(basic.rgb(255, 0, 0))
+    } else if (input.temperature() < 15) {
+        basic.setLedColor(basic.rgb(0, 0, 255))
     } else {
         basic.setLedColor(basic.rgb(0, 255, 0))
-    }
-    if (input.temperature() < 15) {
-        basic.setLedColor(basic.rgb(0, 0, 255))
     }
     basic.showString("" + (input.temperature()))
 })
